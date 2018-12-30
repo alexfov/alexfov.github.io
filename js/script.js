118f3438f4423e0e625d96ecd8bf2e3dacc771bf
alexfov <39302602+alexfov@users.noreply.github.com>
2018-12-31 00:57:37 +0300

    Add files via upload
    table changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let players = {
 	Neeeeeerf: 102756891,
 	JohnGalt: 41528404,
@@ -11,7 +13,7 @@ let monthes = ["Январь", "Февраль", "Март", "Апрель", "М
 let request = new XMLHttpRequest();
 
 let games = {};
-for (key in players){
+for (let key in players){
 	request.open('GET', `https://api.opendota.com/api/players/${players[key]}/matches`, false);
 	request.send();
 	games[key] = JSON.parse(request.responseText);
@@ -21,7 +23,7 @@ for (key in players){
 		request.send();
 let heroes = JSON.parse(request.responseText);
 let heroes_img = {};
-
+//make hero img Map
 for(let hero in heroes){
 	heroes_img[heroes[hero]['localized_name']] = `https://api.opendota.com/apps/dota2/images/heroes/${heroes[hero].name.replace('npc_dota_hero_', '')}_full.png?`
 }
@@ -109,50 +111,62 @@ function map (array, month, year){
 
 function addElement (element, entrails, ..._class) {
 	let newElem = document.createElement(element);
-	newElem.classList.add(..._class);
-	newElem.innerHTML = entrails;
+	if(arguments.length > 1) newElem.innerHTML = entrails;
+	if(arguments.length > 2) newElem.classList.add(..._class);
 	return newElem;
 }
 
-let table_container = document.querySelector('.table-container')
+let table_body = document.querySelector('.table__body');
 
 function drawTable (x, month, year){
-	let cur_month = {};
-	for(key in games){
+	function sorting(a, b){
+		if(a.hero > b.hero) return 1;
+		if(a.hero < b.hero) return -1;
+		return 0;
+	}
+
+	function count_hero_winrate(arr){
+		let obj = {};
+		for(let i in arr){
+			let hero = arr[i];
+			if(!(hero.hero in obj)) {
+				if(hero.win) obj[hero.hero] = [1, 0];
+				else obj[hero.hero] = [0, 1];
+			}
+			else{
+				if(hero.win) obj[hero.hero][0]++;
+				else obj[hero.hero][1]++;
+			}
+		}
+		return obj;
+	}
+
+	let cur_month = {}, row, cell;
+	for(let key in games){
 		cur_month[key] = map(games[key], month, year);
 	}
-	table_container.innerHTML = '';
-	x += 4;
-	let table = addElement('table', '', 'table');
-	table_container.appendChild(table);
-	let header = 'table__header'; //class for cells
-
-	for (let j = 0; j <= Object.keys(players).length + 1; j++) {
-		if(j > 0) header = 'table__cell';
-		let currentPlayer = Object.keys(players)[j - 2];
-
-		for(let i = 0; i < x; i++){
-			//row
-			if(i == 0){
-				var row = addElement('tr', '', 'table__row');
-				table.appendChild(row);
+	//clear table
+	table_body.innerHTML = '';
+	//set current month
+	let table_month = document.querySelector('.table__month');
+	table_month.innerHTML = `${monthes[month - 1]} ${year}`;
+	//go through rows
+	for (let j = 0; j < Object.keys(players).length; j++) {
+		let currentPlayer = Object.keys(players)[j];
+		//go through columns
+		for(let i = 0; i <= 34; i++){
+			//add row
+			if(i === 0){
+				row = addElement('tr', '', 'table__row');
+				table_body.appendChild(row);
 			} 
-			//rows -- end
-			//cells
-			let cell = addElement('td', '', header);
-
-				//print 2nd row
-				if(j == 1 && i > 0 & i < x - 3)	cell.innerHTML = i;
-				if(j == 1 && i == x - 3) cell.innerHTML = 'W';
-				if(j == 1 && i == x - 2) cell.innerHTML = 'L';
-				if(j == 1 && i == x - 1) cell.innerHTML = 'WR';
-
-				//print 1st column
-				if(i == 0 && j > 1) cell.innerHTML = currentPlayer;
+			//dates
+			cell = addElement('td', '', 'table__cell');
+			//set player names
+			if(i === 0) cell.innerHTML = currentPlayer;
 			row.appendChild(cell);
-			//cels -- end
-			//spans
-			if(j > 1 && i > 0){
+			//spans for win/lose colors
+			if(i > 0){
 				//win
 				var spanWin = addElement('span', '', 'table__win');
 				cell.appendChild(spanWin);
@@ -161,28 +175,41 @@ function drawTable (x, month, year){
 				cell.appendChild(spanLose);
 				//add data to cell
 				//days
-				let this_date = 0, wins = 0, loses = 0;
-				if(i < x - 3){
+				let this_date = 0, wins = 0, loses = 0, heroes_winrate;
+				if(i < 34 - 2){
 					this_date = cur_month[currentPlayer].filter(x => x.date[0] == i);
-					wins = this_date.filter(x => x.win);
-					loses = this_date.filter(x => !x.win);
+					wins = this_date.filter(x => x.win).sort(sorting);
+					loses = this_date.filter(x => !x.win).sort(sorting);
 				}
 				else{
-					if(i === x - 3){
-						wins = cur_month[currentPlayer].filter(x => x.win);
+					cell.classList.add('table__cell_bold');
+					wins = cur_month[currentPlayer].filter(x => x.win).sort(sorting);
+					loses = cur_month[currentPlayer].filter(x => !x.win).sort(sorting);
+					if(i === 34 - 2){ //wins colimn
 						loses = [];
 					}
-					if(i === x - 2){
+					if(i === 34 - 1){ //loses column
+						loses = cur_month[currentPlayer].filter(x => !x.win).sort(sorting);
 						wins = [];
-						loses = cur_month[currentPlayer].filter(x => !x.win);
 					}
-					if(i === x - 1){	
+					if(i === 34){		//winrate column
+						let total_games = wins.slice();
+						total_games.push(...loses);
+						heroes_winrate = count_hero_winrate(total_games);
+
 						wins = [];
 						loses = [];
-						cell.innerHTML = ~~(cur_month[currentPlayer].filter(x => x.win).length / cur_month[currentPlayer].length * 100);
+						let winrate = ~~(cur_month[currentPlayer].filter(x => x.win).length / cur_month[currentPlayer].length * 100);
+						if(winrate >= 50) cell.classList.add('table__cell_win');
+						else cell.classList.add('table__cell_lose');
+						cell.innerHTML = winrate;
+
+						let popup = addElement('div', '', 'table__popup');
+						cell.appendChild(popup);
+						drawWinratePopup(heroes_winrate, popup);
 					}
 				}
-				//not for last colum - winrate
+
 				if(wins.length != 0)	spanWin.innerHTML = wins.length;
 				if(loses.length != 0)	spanLose.innerHTML = loses.length;
 				//popup
@@ -200,6 +227,33 @@ function drawTable (x, month, year){
 					popup_link.innerHTML = arr[i].stats;			
 				}
 
+				function drawWinratePopup (obj, popup) {
+					for(let hero in obj){
+						let wins = obj[hero][0];
+						let loses = obj[hero][1];
+						let winrate = ~~(wins * 100 / (wins + loses));
+						let cell_color = 'table__popup-item_win';
+						if (winrate < 50) cell_color = 'table__popup-item_lose';
+
+						let popup_item = addElement('div', '', 'table__popup-item', cell_color);
+						popup.appendChild(popup_item);
+
+						let popup_img = addElement('img', '', 'table__hero-img');
+						popup_img.setAttribute('src', heroes_img[hero]);
+						popup_item.appendChild(popup_img);
+
+						let span = addElement('span', wins + ' - ', 'table__win');
+						popup_item.appendChild(span);
+
+						span = addElement('span', loses + ' - ', 'table__lose');
+						popup_item.appendChild(span);
+
+						winrate < 50 ? cell_color = 'table__lose' : cell_color = 'table__win';
+						span = addElement('span', winrate + '%', cell_color);
+						popup_item.appendChild(span);
+					}
+				}
+
 				if(wins.length != 0 || loses.length != 0){
 					let popup = addElement('div', '', 'table__popup');
 					cell.appendChild(popup);
@@ -210,19 +264,23 @@ function drawTable (x, month, year){
 						for(let i in loses)	drawPopup(i, popup, loses, 'lose');
 				}
 			}
-			//spans--end
-			//heading
-			if(j === 0){
-				cell.setAttribute('colspan', x);
-				cell.innerHTML = `${monthes[month - 1]} ${year}`;
-				break;
-			}
-			//heading -- end
 		}
 	}
 }
+
 let today = new Date();
-drawTable(31, today.getMonth() + 1, today.getFullYear())
+drawTable(31, today.getMonth() + 1, today.getFullYear());
+
+function drawSelectYear (){
+	let select_year = document.querySelector('.select-year');
+	for (var i = today.getFullYear(); i >= 2014 ; i--) {
+		let option = addElement('option', i);
+		option.setAttribute('value', i);
+		select_year.appendChild(option);
+	}
+}
+
+drawSelectYear()
 
 let btn = document.querySelector('.btn');
 
