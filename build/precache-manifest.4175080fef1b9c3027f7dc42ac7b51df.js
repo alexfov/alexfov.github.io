b5b0fb259a2aee6c53c2404318670073abde6c4d
alexfov <39302602+alexfov@users.noreply.github.com>
2020-09-18 10:37:53 +0300

    Add files via upload

---
+++ build/precache-manifest.4175080fef1b9c3027f7dc42ac7b51df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86754b092a415ed36ebea251343ef94b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2374d33b1e91815cb48",
+    "url": "/static/css/main.933d1deb.chunk.css"
+  },
+  {
+    "revision": "0c4520f4348637ad9376",
+    "url": "/static/js/2.5d57d1bf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5d57d1bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2374d33b1e91815cb48",
+    "url": "/static/js/main.ff325faf.chunk.js"
+  },
+  {
+    "revision": "9348aa729fb563783e77",
+    "url": "/static/js/runtime-main.7f2b5c62.js"
+  }
+]);
